06be7db0217c34900d6c83eac5f1c0eef5477612
Matheus Valenzuela <matheuszvalenz@gmail.com>
2025-03-26 23:11:42 +0000

    working on the frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import Navbar from './components/Navbar'
+import { Routes, Route } from 'react-router-dom'
+import ProfilePage from './pages/ProfilePage'
+import SettingsPage from './pages/SettingsPage'
+import LoginPage from './pages/LoginPage'
+import SignUpPage from './pages/SignUpPage'
+import HomePage from './pages/HomePage'
+import { useAuthStore } from '../store/useAuthStore'
 
-const Test = () => {
+const App = () => {
+
+   const {authUser,checkAuth} = useAuthStore()
+
+   useEffect(() => {checkAuth()}, [checkAuth])
+
+   
+  
   return (
-    <div className='Container bg-red-500'>
-      <h1 className=''>Test </h1>
+    <div>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<HomePage/>}/>
+        <Route path='/signup' element={<SignUpPage/>}/>
+        <Route path='/login' element={<LoginPage/>}/>
+        <Route path='settings' element={<SettingsPage/>}/>
+        <Route path='/profile' element={<ProfilePage/>}/>
+
+      </Routes>
     </div>
   )
 }
 
-export default Test
+export default App
