50f0b439886a8e11d2e7a5a95303a3ebeeabfaa1
Matheus Valenzuela <matheuszvalenz@gmail.com>
2025-03-28 11:11:13 +0000

    \

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import { useAuthStore } from '/store/useAuthStore.js' 
+import { Link } from 'react-router-dom'
+import { MessageSquare } from 'lucide-react'
 
 const Navbar = () => {
+  const {logout, authUser} = useAuthStore()
   return (
-    <div>Navbar</div>
+     <header className=' border-b border-base-300 fixed w-full top-0 z-40 backdrop-blur-lg bg-base-100/80'>
+        <div className='container mx-auto px-4 h-16 '>
+          <div className='flex justify-between items-center h-full'> 
+            <Link to="/" className='flex items-center gap-2.5 hover:opacity-80 transition-all'>
+            <div className='w-9 h-9 rounded-lg bg-primary/10 flex items-center justify-center '>
+              <MessageSquare/>
+            </div>
+            <h1 className='w-5 h-5 text-primary'> Chattup</h1>
+            </Link>
+
+          </div>
+        </div>
+     </header>
   )
 }
 
--- frontend/store/useAuthStore.js
+++ frontend/store/useAuthStore.js
@@ -1,5 +1,6 @@
 import {create} from 'zustand';
 import { axiosInstance } from '../src/lib/axios.js';
+import { toast } from 'react-hot-toast';
 
 export const useAuthStore = create((set) => ({
 
@@ -25,16 +26,16 @@ export const useAuthStore = create((set) => ({
     },
 
     signup: async (data) => {
-        set({ isSigningUp: true });
-        try {
-          const res = await axiosInstance.post("/auth/signup", data);
-          set({ authUser: res.data });
-          toast.success("Account created successfully");
+      set({ isSigningUp: true });
+      try {
+        const res = await axiosInstance.post("/auth/signup", data);
+        set({ authUser: res.data });
+        toast.success("Account created successfully");
         
-        } catch (error) {
-          toast.error(error.response.data.message);
-        } finally {
-          set({ isSigningUp: false });
-        }
-      },
+      } catch (error) {
+        toast.error(error.response.data.message);
+      } finally {
+        set({ isSigningUp: false });
+      }
+    },
 }))
