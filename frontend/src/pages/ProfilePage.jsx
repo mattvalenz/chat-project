7cf1cb939e9196f6589fe08c4755257a16fd5ee0
Matheus Valenzuela <matheuszvalenz@gmail.com>
2025-04-01 14:01:12 +0100

    \\

--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -1,30 +1,41 @@
-import React from 'react'
-import { useAuthStore } from '../../store/useAuthStore'
-import { Camera, Mail, User } from 'lucide-react'
+import React, { useState } from "react";
+import { useAuthStore } from "../../store/useAuthStore";
+import { Camera, Mail, User } from "lucide-react";
 
 const ProfilePage = () => {
+  const { authUser, isUpdatingProfile, updateProfile } = useAuthStore();
 
-   const {authUser,isUpdatingProfile, updateProfile} = useAuthStore()
+  const [selectedImg, setSelectedImg] = useState(null)
 
-   const handleImageUpload = async (e) => {
+  const handleImageUpload = async (e) => {
+    const file = e.target.files[0];
+    if (!file) return;
 
-   }
+    const reader = new FileReader();
+
+    reader.readAsDataURL(file);
+
+    reader.onload = async() => {
+      const base64Image = reader.result;
+      setSelectedImg(base64Image)
+      await updateProfile({profilePic: base64Image});
+    }
+  };
   return (
-    <div className='h-screen pt-20'>
-      <div className='max-w-2xl mx-auto p-4 py-8'>
-        <div className='bg-base-300 rounded-xl p-6 space-y-8'>
-        <div className="text-center">
+    <div className="h-screen pt-20">
+      <div className="max-w-2xl mx-auto p-4 py-8">
+        <div className="bg-base-300 rounded-xl p-6 space-y-8">
+          <div className="text-center">
             <h1 className="text-2xl font-semibold ">Profile</h1>
             <p className="mt-2">Your profile information</p>
           </div>
-
 
           {/* avatar upload section */}
 
           <div className="flex flex-col items-center gap-4">
             <div className="relative">
               <img
-                src={authUser.profilePic || "/avatar.png"}
+                src={selectedImg || authUser.profilePic || "/avatar.png"}
                 alt="Profile"
                 className="size-32 rounded-full object-cover border-4 "
               />
@@ -35,7 +46,9 @@ const ProfilePage = () => {
                   bg-base-content hover:scale-105
                   p-2 rounded-full cursor-pointer 
                   transition-all duration-200
-                  ${isUpdatingProfile ? "animate-pulse pointer-events-none" : ""}
+                  ${
+                    isUpdatingProfile ? "animate-pulse pointer-events-none" : ""
+                  }
                 `}
               >
                 <Camera className="w-5 h-5 text-base-200" />
@@ -50,7 +63,9 @@ const ProfilePage = () => {
               </label>
             </div>
             <p className="text-sm text-zinc-400">
-              {isUpdatingProfile ? "Uploading..." : "Click the camera icon to update your photo"}
+              {isUpdatingProfile
+                ? "Uploading..."
+                : "Click the camera icon to update your photo"}
             </p>
           </div>
           <div className="space-y-6">
@@ -59,7 +74,9 @@ const ProfilePage = () => {
                 <User className="w-4 h-4" />
                 Full Name
               </div>
-              <p className="px-4 py-2.5 bg-base-200 rounded-lg border">{authUser?.fullName}</p>
+              <p className="px-4 py-2.5 bg-base-200 rounded-lg border">
+                {authUser?.fullName}
+              </p>
             </div>
 
             <div className="space-y-1.5">
@@ -67,7 +84,9 @@ const ProfilePage = () => {
                 <Mail className="w-4 h-4" />
                 Email Address
               </div>
-              <p className="px-4 py-2.5 bg-base-200 rounded-lg border">{authUser?.email}</p>
+              <p className="px-4 py-2.5 bg-base-200 rounded-lg border">
+                {authUser?.email}
+              </p>
             </div>
           </div>
 
@@ -84,12 +103,10 @@ const ProfilePage = () => {
               </div>
             </div>
           </div>
-
         </div>
-
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ProfilePage
+export default ProfilePage;
