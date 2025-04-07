79f0760062fc3341a17c06512d465a17a3a6f95b
Matheus Valenzuela <matheuszvalenz@gmail.com>
2025-04-07 23:18:16 +0100

    \\

--- frontend/src/components/MessageInput.jsx
+++ frontend/src/components/MessageInput.jsx
@@ -10,13 +10,40 @@ const MessageInput = () => {
 
   const handleImageChange = (e) => {
 
+    const file = e.target.files[0];
+    if (!file.type.startWith("image/")){
+      toast.error("Please select an image file")
+      return
+    }
+    const reader = new FileReader()
+    reader.onloadend= () => {
+      setImagePreview(reader.result);
+    };
+    reader.readAsDataURL(file)
+
   }
 
   const removeImage = () => {
+    setImagePreview(null)
+    if(fileInputRef.current) fileInputRef.current.value = "";
 
   }
 
   const handleSendMessage = async(e) => {
+    e.preventDefault()
+    if(!text.trim ()&& !imagePreview) return
+    try {
+      await sendMessage({
+        text: text.trim(),
+        image: imagePreview
+      })
+      setText("")
+      setImagePreview(null)
+      if (fileInputRef.current) fileInputRef.current.value = "";
+    } catch (error) {
+      console.error("Failed to send message:", error)
+      
+    }
 
   }
 
