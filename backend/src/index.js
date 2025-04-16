60032daa2dcbee95849041f50c4caccaebbf7205
Matheus Valenzuela <matheuszvalenz@gmail.com>
2025-04-16 15:32:09 +0100

    changing pathing to be dynamic, adding conditions for dev/prod

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,11 +8,13 @@ import cors from "cors"
 import cookieParser from "cookie-parser"
 import { app, server } from './lib/socket.js';
 
+import path from "path"
 
 
 dotenv.config()
 
 const PORT = process.env.PORT
+const __dirname = path.resolve()
 
 app.use(express.json())
 app.use(cookieParser())
@@ -23,6 +25,14 @@ app.use(cors(
 
 app.use("/api/auth", authRoutes)
 app.use("/api/messages", messageRoutes)
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, "../frontend/dist")))
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
+    })
+}
 
 server.listen(PORT, () => {
     console.log("server is running on PORT:"+ PORT)
